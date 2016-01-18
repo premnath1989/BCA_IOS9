d6fa7dcc65aa643f759cc83cdac8a40d3dd69b58
Premnath <Premnath@prems-MacBook-Pro-2.local>
2016-01-18 12:23:48 +0800

    CustomUI

---
+++ HLA/CustomUI/myLable.h
@@ -0,0 +1,13 @@
+//
+//  myLable.h
+//  HLA_CA
+//
+//  Created by Danial D. Moghaddam on 3/17/14.
+//  Copyright (c) 2014 Danial D. Moghaddam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myLable : UILabel
+
+@end
---
+++ HLA/CustomUI/myTextView.h
@@ -0,0 +1,13 @@
+//
+//  myTextView.h
+//  HLA_CA
+//
+//  Created by Danial D. Moghaddam on 3/17/14.
+//  Copyright (c) 2014 Danial D. Moghaddam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myTextView : UITextView
+
+@end
