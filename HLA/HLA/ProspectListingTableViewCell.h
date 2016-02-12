7bbb4bf6c291e13dde3d0fb44c190b3dddd0a798
Faiz Umar Baraja <faiz.baraja@infoconnect.com.my>
2016-02-12 12:17:31 +0800

    Add New ProspectListing TableViewCell

---
+++ HLA/HLA/ProspectListingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ProspectListingTableViewCell.h
+//  BLESS
+//
+//  Created by Basvi on 2/12/16.
+//  Copyright © 2016 Hong Leong Assurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProspectListingTableViewCell : UITableViewCell
+
+@end
