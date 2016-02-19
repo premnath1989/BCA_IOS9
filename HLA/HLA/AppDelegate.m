9dc7eb2340495ffe92bb577238b25e7e461d835a
Erwin <erwin.lim@infoconnect.com.my>
2016-02-19 10:41:57 +0800

    add session management, current timeout is 20 mins

--- HLA/HLA/AppDelegate.m
+++ HLA/HLA/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "ClearData.h"
 #import "DBUpdater.h"
+#import "SessionManagement.h"
+#import "Login.h"
 
 @implementation AppDelegate
 @synthesize indexNo;
@@ -44,6 +46,10 @@ NSString *uatAgentCode;
     SIListingIndex = 2;
     NewSIIndex = 3;
     ExitIndex = 4;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidTimeout:) name:kApplicationDidTimeoutNotification object:nil];
+
+    
 #ifdef UAT_BUILD
     if (uatAgentCode != NULL && ![uatAgentCode isEqualToString:@"A8888888"]) {
 #endif
@@ -55,6 +61,25 @@ NSString *uatAgentCode;
     DBUpdater *dbupdater = [[DBUpdater alloc]init];
     [dbupdater updateDatabase];
     return YES;
+}
+
+-(void)applicationDidTimeout:(NSNotification *) notif
+{
+    NSLog (@"time exceeded!!");
+    
+    //This is where storyboarding vs xib files comes in. Whichever view controller you want to revert back to, on your storyboard, make sure it is given the identifier that matches the following code. In my case, "mainView". My storyboard file is called MainStoryboard.storyboard, so make sure your file name matches the storyboardWithName property.
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    while (topController.presentedViewController) {
+        topController = topController.presentedViewController;
+    }
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+    UIViewController *mainLogin = [storyboard instantiateViewControllerWithIdentifier:@"Login"];
+    
+    if(![topController isKindOfClass:[Login class]]){
+        [topController presentViewController:mainLogin animated:YES completion:NULL];
+    }
 }
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *) url {
--- HLA/HLA/main.m
+++ HLA/HLA/main.m
@@ -7,12 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SessionManagement.h"
 
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv,NSStringFromClass([SessionManagement class]), NSStringFromClass([AppDelegate class]));
     }
 }
