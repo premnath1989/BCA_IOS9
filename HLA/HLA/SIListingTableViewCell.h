cdeec7f214356ca83853bcbb2c0154afeb9f7a57
Faiz Umar Baraja <faiz.baraja@infoconnect.com.my>
2016-02-27 22:16:50 +0800

    Display format for SI Listing

---
+++ HLA/HLA/SIListingTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  SIListingTableViewCell.h
+//  BLESS
+//
+//  Created by Basvi on 2/26/16.
+//  Copyright © 2016 Hong Leong Assurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SIListingTableViewCell : UITableViewCell{
+
+}
+@property (nonatomic, weak) IBOutlet UILabel* labelIlusrationNo;
+@property (nonatomic, weak) IBOutlet UILabel* labelIlustrationDate;
+@property (nonatomic, weak) IBOutlet UILabel* labelPOName;
+@property (nonatomic, weak) IBOutlet UILabel* labelProduct;
+@property (nonatomic, weak) IBOutlet UILabel* labelSumAssured;
+@property (nonatomic, weak) IBOutlet UILabel* labelStatus;
+@end
