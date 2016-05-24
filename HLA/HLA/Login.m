1fcb13a0fb4a4e5bb26c502e4b59951bd97b1c93
Erwin Lim <erwin.lim@infoconnect.com.my>
2016-05-24 10:40:58 +0700

    comment the disclaimer

--- HLA/HLA/Login.m
+++ HLA/HLA/Login.m
@@ -646,17 +646,17 @@ static NSString *labelVers;
 - (void) openHome
 {
     
-    AppDisclaimer *disclaimerContent= [[AppDisclaimer alloc] initWithNibName:@"AppDisclaimer"
-                                                                      bundle:nil];
-    disclaimerContent.delegate = self;
-    UIPopoverController *disclaimer = [[UIPopoverController alloc] initWithContentViewController:disclaimerContent];
-    
-    [disclaimer setPopoverContentSize:CGSizeMake(600, 600)];
-    [disclaimer presentPopoverFromRect:CGRectMake(150, 50, 700, 748) inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+//    AppDisclaimer *disclaimerContent= [[AppDisclaimer alloc] initWithNibName:@"AppDisclaimer"
+//                                                                      bundle:nil];
+//    disclaimerContent.delegate = self;
+//    UIPopoverController *disclaimer = [[UIPopoverController alloc] initWithContentViewController:disclaimerContent];
+//    
+//    [disclaimer setPopoverContentSize:CGSizeMake(600, 600)];
+//    [disclaimer presentPopoverFromRect:CGRectMake(150, 50, 700, 748) inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
 
-//    CarouselViewController *carouselMenu = [self.storyboard instantiateViewControllerWithIdentifier:@"carouselView"];
-//    carouselMenu.getInternet = @"No";
-//    [self presentViewController:carouselMenu animated:YES completion:Nil];
+    CarouselViewController *carouselMenu = [self.storyboard instantiateViewControllerWithIdentifier:@"carouselView"];
+    carouselMenu.getInternet = @"No";
+    [self presentViewController:carouselMenu animated:YES completion:Nil];
 }
 
 - (int)syncDaysLeft{
