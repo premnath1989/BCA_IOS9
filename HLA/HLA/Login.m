f82430546e8e7246aea1a84c9427948807631239
Erwin Lim <erwin.lim@infoconnect.com.my>
2016-07-21 15:15:12 +0700

    add check internet before sending Error Report

--- HLA/HLA/Login.m
+++ HLA/HLA/Login.m
@@ -444,23 +444,28 @@ static NSString *labelVers;
 }
 
 - (IBAction)btnReport:(id)sender {
-    NSMutableDictionary *agentDetails = [loginDB getAgentDetails];
-    //compose the agent Details into NSString
-    NSString *agentDetailsStr = @" ";
-    for(NSString *key in agentDetails.allKeys){
-        agentDetailsStr = [agentDetailsStr stringByAppendingString:key];
-        agentDetailsStr = [agentDetailsStr stringByAppendingString:[NSString stringWithFormat:@"= %@ \n ",[agentDetails valueForKey:key]]];
+    if([self connected]){
+        NSMutableDictionary *agentDetails = [loginDB getAgentDetails];
+        //compose the agent Details into NSString
+        NSString *agentDetailsStr = @" ";
+        for(NSString *key in agentDetails.allKeys){
+            agentDetailsStr = [agentDetailsStr stringByAppendingString:key];
+            agentDetailsStr = [agentDetailsStr stringByAppendingString:[NSString stringWithFormat:@"= %@ \n ",[agentDetails valueForKey:key]]];
+        }
+        
+        NSString *dbVersion = [NSString stringWithFormat:
+                               @"%@",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"dbVersion"]];
+        NSString *BCAversion= [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+        NSString *build= [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+        NSString *iosVersion = [[UIDevice currentDevice]systemVersion];
+        
+        NSString *ErrorLog = [NSString stringWithFormat:@"%@APP Version : %@ %@ \n iOS Version : %@ \n DB Version : %@ \n Data Version : ", agentDetailsStr,BCAversion, build, iosVersion, dbVersion];
+        [self writeStringToFile:ErrorLog];
+        [self sendByEmail:agentDetails];
+    }else{
+        UIAlertView* alert = [[UIAlertView alloc]initWithTitle:@"No Internet" message:[NSString stringWithFormat:@"Periksa lagi internet anda untuk mengirim error report"] delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
     }
-    
-    NSString *dbVersion = [NSString stringWithFormat:
-                           @"%@",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"dbVersion"]];
-    NSString *BCAversion= [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
-    NSString *build= [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
-    NSString *iosVersion = [[UIDevice currentDevice]systemVersion];
-    
-    NSString *ErrorLog = [NSString stringWithFormat:@"%@APP Version : %@ %@ \n iOS Version : %@ \n DB Version : %@ \n Data Version : ", agentDetailsStr,BCAversion, build, iosVersion, dbVersion];
-    [self writeStringToFile:ErrorLog];
-    [self sendByEmail:agentDetails];
     
     
 }
@@ -1098,12 +1103,7 @@ static NSString *labelVers;
     // Release any retained subviews of the main view.
 }
 
-- (BOOL)IsConnected
-{
-    Reachability *reachability = [Reachability reachabilityForInternetConnection];
-    NetworkStatus networkStatus = [reachability currentReachabilityStatus];
-    return networkStatus != NotReachable;
-}
+
 
 - (BOOL)alertViewShouldEnableFirstOtherButton:(UIAlertView *)alertView
 {
