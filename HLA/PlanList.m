9670935d4ada8fdef67f96d29cf1942e459590a1
Faiz Baraja <faiz.baraja89@gmail.com>
2017-06-13 08:44:05 +0700

    Commit 1. Hide Unit Link For IPA 10 - 20 2. Disable menu for CFF, SAM, & SPAJ 3. Disable menu For Backup,Restore, Sync & SPAJ Number

--- HLA/PlanList.m
+++ HLA/PlanList.m
@@ -29,11 +29,11 @@
 		ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"Secure100", @"HLA Wealth Plan", nil ];
 		ListOfCode = [[NSMutableArray alloc] initWithObjects:@"S100", @"HLAWP", nil ];
         
-        ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"BCA Life Heritage Protection",@"BCA Life Keluargaku",@"BCA Life Heritage Protection - For BCA Staff",@"BCA Life Proteksi & Investasiku",nil];
-        ListOfCode = [[NSMutableArray alloc] initWithObjects:@"BCALH",@"BCAKK",@"BCALHST",@"BCALUL", nil ];
+        //ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"BCA Life Heritage Protection",@"BCA Life Keluargaku",@"BCA Life Heritage Protection - For BCA Staff",@"BCA Life Proteksi & Investasiku",nil];
+        //ListOfCode = [[NSMutableArray alloc] initWithObjects:@"BCALH",@"BCAKK",@"BCALHST",@"BCALUL", nil ];
         
-        //ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"BCA Life Heritage Protection",@"BCA Life Keluargaku",@"BCA Life Heritage Protection – For BCA Staff",nil];
-        //ListOfCode = [[NSMutableArray alloc] initWithObjects:@"BCALH",@"BCAKK",@"BCALHST", nil ];
+        ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"BCA Life Heritage Protection",@"BCA Life Keluargaku",@"BCA Life Heritage Protection – For BCA Staff",nil];
+        ListOfCode = [[NSMutableArray alloc] initWithObjects:@"BCALH",@"BCAKK",@"BCALHST", nil ];
         
 //		ListOfPlan = [[NSMutableArray alloc] initWithObjects:@"HLA Wealth Plan", nil ];
 //		ListOfCode = [[NSMutableArray alloc] initWithObjects:@"HLAWP", nil ];
